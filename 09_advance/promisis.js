36730eb7474b47b4d29da0e5e802170ccacb30b6
Ankushydv4005 <ankushyadav4775@gmail.com>
2025-03-28 18:31:23 +0000

    promisis

---
+++ 09_advance/promisis.js
@@ -0,0 +1,53 @@
+// create the promice
+const promiceOne=new Promise(function(resolve,reject)
+{
+    // Do an asnc task
+    // Bata base call like data connect,receive,cryptography,network
+    setTimeout(function( )
+{
+    console.log('Async task is completed');
+    resolve();    //for connection 
+
+},1000);
+ 
+})
+
+promiceOne.then(function()
+{
+    console.log(" promice consumed");
+})
+
+
+
+// eako hm kisi variabel me store kiye bina bhi ho sakta h 
+
+new Promise(function(resolve,reject)
+{
+    setTimeout  (function()
+{
+    console.log("Async task 2");
+    resolve();
+},1000)
+
+}).then(function()
+{
+    console.log("Async 2 resolved");
+})
+
+
+ 
+
+const PromiceThree=new Promise(function(resolve,reject)
+{
+    setTimeout(function()
+    {
+        resolve({userName:"Ankush Yadav",email:"ankushyadav4005@gmail.com"})
+    },1000)
+});
+
+
+PromiceThree.then(function(user)
+{
+    console.log(user);
+});
+
