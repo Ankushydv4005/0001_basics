a6892218f96b825eee9ec01deec8850a61caf1f8
Ankushydv4005 <ankushyadav4775@gmail.com>
2025-03-29 10:13:55 +0000

    promise

--- 09_advance/promisis.js
+++ 09_advance/promisis.js
@@ -39,9 +39,9 @@ new Promise(function(resolve,reject)
 
 const PromiceThree=new Promise(function(resolve,reject)
 {
-    setTimeouts(function()
+    setTimeout(function()
     {
-        resolve({userName:"Ankush Yadav",email:"ankushyadav4005@gmail.com"})
+        resolve({userName:"Ankush Yadav",email:"ankushyadav4005@gmail.com"})  // array ,object ,function kuchh bhi pass kar skte h 
     },1000)
 });
 
@@ -50,4 +50,95 @@ PromiceThree.then(function(user)
 {
     console.log(user);
 });
+
+
+
+
+const promiseFour=new Promise(function(resolve,reject)
+{
+    setInterval(function()
+    {
+        
+
+        let error=false;
+        if(!error)
+        {
+            resolve({username:"Ankush",password : "123"})
+        }
+        else{
+            reject('Error : Something   went wrong ')
+        }
+
+
+    },1000)
+})
+
+
+promiseFour.then((user)=>
+{
+    console.log(user);
+    return user.username;
+}) .then((username)=>
+{
+   console.log(username);
+}).catch((error)=>{
+    console.log(error);
+}).finally(()=>
+{
+      console.log("the promice is either resolve or rejected ");
+})
+
+
+
+
+
+
+const promiseFive=new Promise(function(resolve,reject)
+{
+
+
+    setTimeout(function()
+    {
+        
+
+        let error=false;
+        if(!error)
+        {
+            resolve({username:"Javascript ",password : "123"});
+        }
+        else{
+            reject('Error : Something js went wrong ')
+        }
+
+
+    },1000)
+
+
+})
+
+async function consumepromisefive()  
+{
+   try{
+    const response= await promiseFive
+   console.log(response);
+   }
+   catch(error)
+   {
+    console.log(error);
+   }
+}
+
+consumepromisefive();
+
+
+async function getAlluser()
+{
+    const response=await fetch('https //jsonplaceholder.typicode.com/users'')
+        const data=response.json();
+        console.log(data);
+}
+getAlluser()
+
+
+
 
